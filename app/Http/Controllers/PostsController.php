b910d1a1b9281d06c07e51922341493731ee3d98
Liza-S <lizash605@gmail.com>
2017-12-23 20:32:58 +0200

    Added editing and deleting posts

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -43,7 +43,7 @@ class PostsController extends Controller
 
         $post = new Post;
         $post->title = $request->input('title');
-        $post->body = $request->input('title');
+        $post->body = $request->input('body');
         $post->save();
         return redirect('/posts')->with('success', 'Post Created');
     }
@@ -68,7 +68,8 @@ class PostsController extends Controller
      */
     public function edit($id)
     {
-        //
+        $post = Post::find($id);
+        return view('posts.edit')->with('post', $post);
     }
 
     /**
@@ -80,7 +81,16 @@ class PostsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+        $post = Post::find($id);
+        $post->title = $request->input('title');
+        $post->body = $request->input('body');
+        $post->save();
+        return redirect('/posts')->with('success', 'Post Updated');
     }
 
     /**
@@ -91,6 +101,8 @@ class PostsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Removed');
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,14 @@
 	<a href="/posts" class="btn btn-default">Go Back</a>
 	<h1>{{$post->title}}</h1>
 	<div>
-		{{$post->body}}
+		{!!$post->body!!}
 	</div>
 	<hr>
 	<small>Written on {{$post->created_at}}</small>
+	<hr>
+	<a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+	{!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+		{{Form::hidden('_method', 'DELETE')}}
+		{{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+	{!!Form::close()!!}
 @endsection
