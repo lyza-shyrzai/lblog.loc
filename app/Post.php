08e2043c18bd3344581fe1fbe6a6975914dab2f2
Liza-S <lizash605@gmail.com>
2017-12-23 22:16:22 +0200

    Added Model Relationship

--- app/Post.php
+++ app/Post.php
@@ -9,4 +9,9 @@ class Post extends Model
     protected $table = 'posts';
     public $primaryKey = 'id';
     public $timestamps = true;
+
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,20 @@
                     @endif
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <h3>Your Blog Posts</h3>
+                    <table class="table table-striped">
+                        <tr>
+                            <th>Title</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                        @foreach ($posts as $post)
+                        <tr>
+                            <th>{{$post->title}}</th>
+                            <th><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></th>
+                            <th></th>
+                        </tr>
+                        @endforeach
+                    </table>
                 </div>
             </div>
         </div>
