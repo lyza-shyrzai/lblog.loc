0f4c0cd51a4465a0af644b03bf86357d2c588d2a
Liza-S <lizash605@gmail.com>
2017-12-25 16:55:54 +0200

    Added Delete button and information about post

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,20 +15,29 @@
                     @endif
                     <a href="/posts/create" class="btn btn-primary">Create Post</a>
                     <h3>Your Blog Posts</h3>
-                    <table class="table table-striped">
-                        <tr>
-                            <th>Title</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                        @foreach ($posts as $post)
-                        <tr>
-                            <th>{{$post->title}}</th>
-                            <th><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></th>
-                            <th></th>
-                        </tr>
-                        @endforeach
-                    </table>
+                    @if (count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                            @foreach ($posts as $post)
+                            <tr>
+                                <td>{{$post->title}}</td>
+                                <td><a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a></td>
+                                <td>
+                                    {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                        {{Form::hidden('_method', 'DELETE')}}
+                                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                    {!!Form::close()!!}
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    @else
+                        <p>You have no posts</p>
+                    @endif
                 </div>
             </div>
         </div>
